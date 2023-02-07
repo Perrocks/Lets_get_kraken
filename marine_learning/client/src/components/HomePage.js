88041297b5ac6d72b8193c931fdce77f6c5995b7
John Panton <johnpanton29@outlook.com>
2023-02-07 12:02:07 +0000

    added some information to homepage

--- marine_learning/client/src/components/HomePage.js
+++ marine_learning/client/src/components/HomePage.js
@@ -2,7 +2,13 @@ import React from "react";
 
 const HomePage = () => {
     return (
-        <h1>HomePage, baby!</h1>
+        <div>
+            <h2>Water we going to do about marine conservation?!</h2>
+            <p>We all know it’s very important to look after the planet. We do, don’t we?!</p>
+            <p>Our oceans are vital to the health of our world. But what can you do to help?</p>
+            <p>Use this website to learn more about our oceans and what lives in them. There are also links to follow to find out more, and learn how you can get involved!</p>
+            <p>Once you’ve read each chunk of information, tick the box to mark it as 'done'. Once everything is 'done', test your knowledge with a quiz!</p>
+        </div>
     )
 }
 
