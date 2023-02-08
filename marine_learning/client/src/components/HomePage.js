1294fdc7ea12f230c28ce7ebabea1bcfa77f0f3d
Indira Borras <borrasindira@gmail.com>
2023-02-08 15:37:07 +0000

    CSS done

--- marine_learning/client/src/components/HomePage.js
+++ marine_learning/client/src/components/HomePage.js
@@ -5,19 +5,18 @@ const HomePage = () => {
     return (
     <section id='section-body'>  
         <div id='home-page-body'>
-            <h2>Water we going to do about marine conservation?!</h2>
-            <p>We all know it’s very important to look after the planet. We do, don’t we?!</p>
-            <p>Our oceans are vital to the health of our world. But what can you do to help?</p>
-            <p>Use this website to learn more about our oceans and what lives in them.</p> 
+            <h2>WATER WE GOING TO DO ABOUT MARINE CONSERVATION?!</h2>
+            <p>We all know it’s very important to look after the planet. <b>We do, don’t we?!</b></p>
+            <p>Our oceans are vital to the health of our world. <b>But what can you do to help?</b></p>
+            <p>Use this website to <b>learn more about our oceans </b> and what lives in them.</p> 
             <br></br>
-            <br></br>
-            <br></br>
-            <p>Read each bubble of information to learn about each topic. <br></br>When you feel you have learned it all, tick the box to mark it as 'done'.
-            <br></br> Once all the bubbles are 'done', test your knowledge with a quiz</p>
-            <br></br>
-            <br></br>
-            <br></br>
-            <p>There are also links to follow to find out more, and learn how you can get involved!</p>
+            <h2>HOW TO PLAY</h2>
+            <ul id='home-list'>
+                <li>1. <b> Read each bubble of information </b> to learn about each topic.</li>
+                <li>2.When you feel you have learned it all, <b>tick the box </b> to mark it as 'done'.</li>
+                <li>3.Once all the bubbles are 'done', <b>test your knowledge with a quiz</b></li>
+            </ul>
+            <p>🐠 🐠 There are also links to follow to find out more, and learn how you can get involved! 🐠 🐠 </p>
         </div>
     </section>  
 
